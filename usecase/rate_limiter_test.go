cb70c11ed548d72be6ed5a430975f503b9c0eec4
Bruno Holanda <br.holanda15@gmail.com>
2024-07-18 21:14:45 -0300

    test: completed test to rate limit usecase

--- usecase/rate_limiter_test.go
+++ usecase/rate_limiter_test.go
@@ -3,6 +3,7 @@ package usecase
 import (
 	"context"
 	"errors"
+	"rate_limiter/domain"
 	"rate_limiter/infra/mocks"
 	"testing"
 
@@ -40,6 +41,88 @@ func TestRateLimiterUseCase_Execute(t *testing.T) {
 
 		assert.Nil(t, output.Err)
 		assert.False(t, output.AllowRequest)
+	})
+
+	t.Run("verify block error", func(t *testing.T) {
+		limiterMock := mocks.NewRateLimiterMock()
+		errBlock := errors.New("error blocking key")
+
+		limiterMock.VerifyKeyBlockFunc = func(ctx context.Context, key string) (bool, error) {
+			return false, errBlock
+		}
+
+		useCase := NewRateLimiterUseCase(limiterMock)
+
+		input := InputRateLimiter{
+			Item: "test-item",
+		}
+
+		output := useCase.Execute(ctx, input)
+
+		assert.Equal(t, errBlock, output.Err)
+	})
+
+	t.Run("allow request", func(t *testing.T) {
+		limiterMock := mocks.NewRateLimiterMock()
+		limiterMock.VerifyKeyBlockFunc = func(ctx context.Context, key string) (bool, error) {
+			return false, nil
+		}
+
+		limiterMock.SetLimitForKeyPerTimeFunc = func (ctx context.Context, key string, duration int, time string) (domain.LimitResult, error) {
+			return domain.LimitResult{Allowed: 1, Remaining: 1}, nil
+		}
+
+		useCase := NewRateLimiterUseCase(limiterMock)
+
+		input := InputRateLimiter{
+			Item: "test-item",
+		}
+
+		output := useCase.Execute(ctx, input)
+		assert.Nil(t, output.Err)
+		assert.Equal(t, true, output.AllowRequest)
+	})
+
+	t.Run("error setting limit", func(t *testing.T) {
+		limiterMock := mocks.NewRateLimiterMock()
+		limiterMock.VerifyKeyBlockFunc = func(ctx context.Context, key string) (bool, error) {
+			return false, nil
+		}
+
+		errLimit := errors.New("error setting limit")
+		limiterMock.SetLimitForKeyPerTimeFunc = func (ctx context.Context, key string, duration int, time string) (domain.LimitResult, error) {
+			return domain.LimitResult{}, errLimit
+		}
+
+		useCase := NewRateLimiterUseCase(limiterMock)
+
+		input := InputRateLimiter{
+			Item: "test-item",
+		}
+
+		output := useCase.Execute(ctx, input)
+		assert.Equal(t, errLimit, output.Err)
+	})
+
+	t.Run("reach requests limit", func(t *testing.T) {
+		limiterMock := mocks.NewRateLimiterMock()
+		limiterMock.VerifyKeyBlockFunc = func(ctx context.Context, key string) (bool, error) {
+			return false, nil
+		}
+
+		limiterMock.SetLimitForKeyPerTimeFunc = func (ctx context.Context, key string, duration int, time string) (domain.LimitResult, error) {
+			return domain.LimitResult{Allowed: 0, Remaining: 0}, nil
+		}
+
+		useCase := NewRateLimiterUseCase(limiterMock)
+
+		input := InputRateLimiter{
+			Item: "test-item",
+		}
+
+		output := useCase.Execute(ctx, input)
+		assert.Nil(t, output.Err)
+		assert.Equal(t, false, output.AllowRequest)
 	})
 
 }
